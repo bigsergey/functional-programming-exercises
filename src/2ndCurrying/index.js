d981e6c3d017d2c95fee66866cbf167f60a2e1c5
Rafał Gruszecki <rgruszecki@apptension.com>
2016-09-26 21:32:59 +0200

    add exercise 2nd with currying

--- src/2ndCurrying/index.js
+++ src/2ndCurrying/index.js
@@ -1,1 +1,15 @@
-export function words () {}
+import R from 'ramda';
+
+export const words = R.split(' ');
+
+export function increaseArray(array, number) {
+  return R.map(R.add(number), array)
+}
+
+const less = (a, b) => {
+  return a < b ? a : b;
+};
+
+export function smallestNumber(array) {
+  return R.reduce(less, array, array)
+}
--- src/main.js
+++ src/main.js
@@ -2,4 +2,12 @@ import 'babel-polyfill';
 
 import {add} from './exampleFunction';
 
+import {words} from './2ndCurrying'
+import {increaseArray} from './2ndCurrying'
+import {smallestNumber} from './2ndCurrying'
+
 console.log(add(1, 3));
+
+console.log(words('a b c'));
+console.log(increaseArray([1,2,3,4], 1));
+console.log(smallestNumber([1,2,3,4]));
