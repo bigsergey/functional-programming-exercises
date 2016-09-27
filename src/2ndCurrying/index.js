03017293bf40e6470bff2607e4b49bf467cc0377
Rafał Gruszecki <rgruszecki@apptension.com>
2016-09-27 20:13:01 +0200

    simplify function and add additional with R.min to find the lowest number in array

--- src/2ndCurrying/index.js
+++ src/2ndCurrying/index.js
@@ -2,14 +2,12 @@ import R from 'ramda';
 
 export const words = R.split(' ');
 
-export function increaseArray(array, number) {
-  return R.map(R.add(number), array)
-}
+export const increaseArray = (number, array) => R.map(R.add(number), array);
 
 const less = (a, b) => {
   return a < b ? a : b;
 };
 
-export function smallestNumber(array) {
-  return R.reduce(less, array, array)
-}
+export const smallestNumber = (array) => R.reduce(less, Infinity, array);
+
+export const smallestNumberWithRamdaMin = (array) => R.reduce(R.min, Infinity, array);
